0b1cea8c937ee994402546b9adde48b32f505c36
Bright <ntpchong_0411@hotmail.com>
2016-03-29 10:55:34 +0700

    first time met git

--- src/GameLayer.js
+++ src/GameLayer.js
@@ -2,7 +2,10 @@ var GameLayer = cc.LayerColor.extend({
   init: function() {
     this._super( new cc.Color( 127, 127, 127, 255 ) );
     this.setPosition( new cc.Point( 0, 0 ) );
-    
+    this.box = new Box();
+    this.box.setPosition( new cc.Point( 28, 85 ) );
+    this.addChild(this.box);
+    this.box.scheduleUpdate();
     return true;
   }
 });
---
+++ src/box.js
@@ -0,0 +1,6 @@
+var Box = cc.Sprite.extend({
+    ctor: function() {
+        this._super();
+        this.initWithFile( 'res/pic/Box.jpg' );
+    }
+});
